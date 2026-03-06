366ccc85e6874a9de4e07f077140e963baa8d6d3
DaSonOfPoseidon <keithleyjj@icloud.com>
2026-03-05 18:25:09 -0600

    test

--- Portal/models/user.py
+++ Portal/models/user.py
@@ -6,3 +6,5 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+    #comment
