819a720830d6f1445ac5f5025d678e7af5d18a7a
Jackson <106688879+DaSonOfPoseidon@users.noreply.github.com>
2026-03-06 15:56:49 -0600

    update with main (#9)
    * Added folder structure to the portal folder
    
    * Update to Training Model
    
    * Added login/register/logout functionality
    
    * test
    
    * deleted "portal" folder
    
    * updated gitignore
    
    * Updated app run
    
    * basic deploy setup
    
    * oops lol i forgot to stage stuff
    
    * fix deploy filepath
    
    ---------
    
    Co-authored-by: Dane Ehlmann <deebq3@umsystem.edu>
    Co-authored-by: DaneEhlmann16 <156849966+DaneEhlmann16@users.noreply.github.com>
    Co-authored-by: Michael McClanahan, MCP <108365481+mcclanahanmp@users.noreply.github.com>
    Co-authored-by: Jackson <dasonofposeidon5@gmail.com>

---
+++ Portal/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_login import LoginManager
+from Portal.config import Config
+from Portal.models import db, User
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from Portal.routes.auth import auth
+    app.register_blueprint(auth)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
